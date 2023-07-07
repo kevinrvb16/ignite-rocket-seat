26f0bc314a4ab87af2ee7878f4cba71857847796
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2023-07-07 18:43:16 -0300

    Primeira aula sobre fundamentos do node js

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import http from 'node:http'
+
+//Criar um usuário(nome, email, senha)
+
+const server = http.createServer((req, res) => {
+    return res.end('Hello World')
+})
+
+server.listen(3333)
