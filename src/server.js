8962fe2a8a4b1ee342cfb281b2d65b4910232cf2
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2023-07-08 13:44:59 -0300

    Aula 1 concluída

--- src/server.js
+++ src/server.js
@@ -1,9 +1,37 @@
 import http from 'node:http'
 
-//Criar um usuário(nome, email, senha)
+// - criar usuários
+// - listar usuários
+// - Edição de usuário
+// - Remoção de usuário
+
+// - HTTP
+//    - métodos HTTP
+//    - url
+
+//cabecalhos (Requisição/resposta) => metadados
+
+// HTTP Status code
+const users = []
 
 const server = http.createServer((req, res) => {
-    return res.end('Hello World')
+    const { method, url } = req
+
+    if (methos === 'GET' && url === '/users') {
+        return res
+        .setHeader('Content-Type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            name: 'John Doe',
+            email: 'johndoe@ex.com'
+        })
+        return res.writeHead(201).end()
+    }
+
+    return res.write(404).end()
 })
 
 server.listen(3333)
---
+++ streams/fundamentals.js
@@ -0,0 +1,14 @@
+// Netflix & Spotify
+
+// Importação de clientes via CSV (Excel)
+// 1Gb - 1.000.000 de linhas
+// POST /upload import.csv
+
+// 10mb/s = 100s
+
+// 100s -> inserções no banco de dados
+
+// 10md/s -> 10.000 
+
+// Readable Stream => leitura de dados
+// Writable Stream => escrita de dados
