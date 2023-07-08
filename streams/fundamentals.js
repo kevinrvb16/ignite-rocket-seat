fe176f6ae68d0337531f748e026777d7930c7a64
KEVIN RAFAEL VELEZ BERNAL <42079591+kevinrvb16@users.noreply.github.com>
2023-07-08 14:08:35 -0300

    Streams aula 2

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,14 +1,40 @@
-// Netflix & Spotify
+// Streams ->
 
-// Importação de clientes via CSV (Excel)
-// 1Gb - 1.000.000 de linhas
-// POST /upload import.csv
+/* process.stdin
+    .pipe(process.stdout) */
 
-// 10mb/s = 100s
+import { Readable, Writable, Transform } from 'node:stream'
 
-// 100s -> inserções no banco de dados
+class OneToHundredStream extends Readable {
+    index = 1
+    _read() {
+        const i = this.index++
 
-// 10md/s -> 10.000 
+        setTimeout(()=> {  
+            if (i > 100) {
+                this.push(null)
+            } else {	
+                const buf = Buffer.from(String(i))
+                this.push(buf)
+            }
+        }, 1000)
+    }
+}
 
-// Readable Stream => leitura de dados
-// Writable Stream => escrita de dados
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * - 1
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write (chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+new OneToHundredStream()
+.pipe(new InverseNumberStream())
+.pipe(new MultiplyByTenStream())
